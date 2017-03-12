ed405d4127db7b2626dcf020b0082be2652771e2
kareem <kareem.elshahawe@gmail.com>
2017-03-12 16:47:36 +0800

    Finish ch1

--- src/ch1/let.js
+++
@@ -1,9 +0,0 @@
-// console.log(x);
-let x = 555;
-
-console.log(x); //Prints 5
-
-for (let i = 0; i < 5; i++) {
-    //Do something
-}
-console.log(i); //Prints 5
--- src/ch1/var.js
+++
@@ -1,9 +0,0 @@
-{
-    var x = 555;
-}
-console.log(x) //Prints 5
-
-for (var i = 0; i < 5; i++) {
-    //Do something
-}
-console.log(i); //Prints 5
